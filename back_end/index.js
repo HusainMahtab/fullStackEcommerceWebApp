ad420ce91c01eb6d62e897c9e5716752e58072aa
ahkhan13 <khanahs3351@gmail.com>
2024-09-19 19:30:53 +0530

    change origin

--- back_end/index.js
+++ back_end/index.js
@@ -10,7 +10,7 @@ dotenv.config({path:"./.env"})
 const app=express()
 
 app.use(cors({
-    origin:"*",
+    origin:"https://fullstackecommercewebapp-1-front-end.onrender.com/",
     credentials:true
 }))
 
